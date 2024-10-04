faa5f487b5afe2eabf5e6f48d50db2824a57873b
JosueIsOffline <josuehernandez2314@gmail.com>
2024-10-04 17:44:12 -0400

    testing deploy with GitActions

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,61 @@
-import { Text, View } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 
 export default function Index() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aquí podrías agregar la lógica de autenticación
+    if (email && password) {
+      console.log(`Login Successful, Email: ${email}\nPassword: ${password}`);
+      // Aquí iría la lógica de autenticación
+    } else {
+      Alert.alert('Error', 'Please enter both email and password');
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <View style={styles.container}>
+    <Text style={styles.title}>Searching</Text>
+    <Text style={styles.title}>Login</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Email"
+      value={email}
+      onChangeText={setEmail}
+      keyboardType="email-address"
+      autoCapitalize="none"
+    />
+    <TextInput
+      style={styles.input}
+      placeholder="Password"
+      value={password}
+      onChangeText={setPassword}
+      secureTextEntry
+    />
+    <Button title="Login" onPress={handleLogin} />
+  </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 24,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+});
